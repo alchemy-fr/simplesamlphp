63d206b9c9c8dc695c8cc5b0c49a5cc0fc6f3c0d
Jean-Yves Gaulier <gaulier@alchemy.fr>
2018-09-17 19:34:59 +0200

    set configuration fix build

--- config-templates/config.php
+++ config-templates/config.php
@@ -73,7 +73,7 @@ $config = array(
      * also as the technical contact in generated metadata.
      */
     'technicalcontact_name' => 'Administrator',
-    'technicalcontact_email' => 'na@example.org',
+    'technicalcontact_email' => 'support@alchemy.fr',
 
     /*
      * The envelope from address for outgoing emails.
@@ -105,7 +105,7 @@ $config = array(
      * A possible way to generate a random salt is by running the following command from a unix shell:
      * LC_CTYPE=C tr -c -d '0123456789abcdefghijklmnopqrstuvwxyz' </dev/urandom | dd bs=32 count=1 2>/dev/null;echo
      */
-    'secretsalt' => 'defaultsecretsalt',
+    'secretsalt' => 'rgvsbfvurgvurz16',
 
     /*
      * This password must be kept secret, and modified from the default value 123.
@@ -113,7 +113,7 @@ $config = array(
      * metadata listing and diagnostics pages.
      * You can also put a hash here; run "bin/pwgen.php" to generate one.
      */
-    'auth.adminpassword' => '123',
+    'auth.adminpassword' => 'alchemy',
 
     /*
      * Set this options to true if you want to require administrator password to access the web interface
